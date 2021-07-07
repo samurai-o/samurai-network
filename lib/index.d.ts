5fb3121ea1acfc71dd7d2a74abc0ef5ef1cb1fd6
刘伟 <liuwei03@dongfangfuli.com>
2021-07-07 13:32:35 +0800

    异常处理优化

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,14 +2,15 @@ import { AxiosInstance } from 'axios';
 export declare type NetworkStatusFunc<D = any> = (data: D) => D;
 export declare class Network {
     private readonly url?;
-    private static client;
-    private static axios;
-    static Get(url: string): (target: Network, key: string, descriptor: any) => any;
-    static Post(url: string): (target: Network, key: string, descriptor: any) => any;
-    constructor(url?: string | undefined);
+    private static instance;
+    private _client;
     private status_monitor;
     private token;
+    static Get(url: string, headers?: any): (_target: Network, _key: string, descriptor: any) => any;
+    static Post(url: string, headers?: any): (_target: Network, _key: string, descriptor: any) => any;
+    static Delete(url: string, headers?: any): (_target: Network, _key: string, descriptor: any) => any;
+    constructor(url?: string | undefined);
     private initial;
     protected status<R = any>(status: number, func: NetworkStatusFunc<R>): void;
-    get send(): AxiosInstance;
+    get client(): AxiosInstance;
 }
--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,48 @@ import { isEmpty, isFunc } from '@frade-sam/samtools';
 export type NetworkStatusFunc<D = any> = (data: D) => D;
 
 export class Network {
-    private static client: Network;
-    private static axios: AxiosInstance;
+    private static instance: Network;
+    private _client: AxiosInstance = axios.create();
+    private status_monitor: Map<number, NetworkStatusFunc> = new Map([]);
+    private token: string = '';
 
-    static Get(url: string) {
-        return (target: Network, key: string, descriptor: any) => {
+    static Get(url: string, headers?: any) {
+        return (_target: Network, _key: string, descriptor: any) => {
             const func = descriptor.value;
-            descriptor.value = async function (params: any) {
-                const res = await Network.client.send({ url, method: 'GET', params });
-                return func.apply(target, [params, res]);
+            descriptor.value = async function (params: any, defaultData?: any) {
+                const response = await Network.instance
+                    .client({ url, method: 'GET', params, headers })
+                    .then((res) => (isEmpty(res) || isEmpty(res.data)) ? ({ ...res, data: defaultData }) : res)
+                    .catch((error) => ({ ...error, data: defaultData }));
+                return func.apply(Network.instance, [params, response]);
             };
             descriptor.writable = false;
             return descriptor;
         };
     }
 
-    static Post(url: string) {
-        return (target: Network, key: string, descriptor: any) => {
+    static Post(url: string, headers?: any) {
+        return (_target: Network, _key: string, descriptor: any) => {
             const func = descriptor.value;
-            descriptor.value = async function (data: any, _res?: any) {
-                const res = await Network.client
-                    .send({ url, method: 'POST', data })
-                    .then((res) => {
-                        if (isEmpty(res) || isEmpty(res.data))
-                            return { ...res, data: _res };
-                        return res;
-                    })
-                    .catch(() => ({ data: _res }));
-                return func.apply(target, [data, res]);
+            descriptor.value = async function (data: any, defaultData?: any) {
+                const response = await Network.instance.client({ url, method: 'POST', data, headers })
+                    .then((res) => (isEmpty(res) || isEmpty(res.data)) ? ({ ...res, data: defaultData }) : res)
+                    .catch((error) => ({ ...error, ...defaultData }));
+                return func.apply(Network.instance, [data, response]);
+            };
+            descriptor.writable = false;
+            return descriptor;
+        };
+    }
+
+    static Delete(url: string, headers?: any) {
+        return (_target: Network, _key: string, descriptor: any) => {
+            const func = descriptor.value;
+            descriptor.value = async function (data: any, defaultData?: any) {
+                const response = await Network.instance.client({ url, method: 'DELETE', data, headers })
+                    .then((res) => (isEmpty(res) || isEmpty(res.data)) ? ({ ...res, data: defaultData }) : res)
+                    .catch((error) => ({ ...error, ...defaultData }));
+                return func.apply(Network.instance, [data, response]);
             };
             descriptor.writable = false;
             return descriptor;
@@ -41,17 +55,16 @@ export class Network {
     constructor(private readonly url?: string) {
         this.initial();
     }
-    private status_monitor: Map<number, NetworkStatusFunc> = new Map([]);
-    private token: string = '';
+
     private initial() {
-        Network.axios = axios.create({ baseURL: this.url });
-        Network.axios.interceptors.request.use((value) => {
+        this._client = axios.create({ baseURL: this.url });
+        this._client.interceptors.request.use((value) => {
             return {
                 ...value,
                 headers: { ...value.headers, authorization: this.token },
             };
         });
-        Network.axios.interceptors.response.use(
+        this._client.interceptors.response.use(
             (res) => {
                 const { status, data, headers } = res;
                 const func = this.status_monitor.get(status);
@@ -60,17 +73,17 @@ export class Network {
                 return func(data);
             },
             () => {
-                return { code: 1, data: null, message: '' };
+                return { code: -1, data: null, message: '网络异常' };
             },
         );
-        if (!Network.client) Network.client = this;
+        if (!Network.instance) Network.instance = this;
     }
 
     protected status<R = any>(status: number, func: NetworkStatusFunc<R>) {
         this.status_monitor.set(status, func);
     }
 
-    get send() {
-        return Network.axios;
+    get client() {
+        return this._client;
     }
 }
